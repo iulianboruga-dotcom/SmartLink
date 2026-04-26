1095c64aa2b88ef19a137b8c1c6d63082681e79a
PaulaTomo <paulatomoiaga91@yahoo.com>
2026-04-26 14:03:14 +0200

    Adaugate validari alarme, istoric, praguri si confirmare rezolvare alarma

--- cloud-backend/routes/patientRoutes.js
+++ cloud-backend/routes/patientRoutes.js
@@ -9,13 +9,13 @@ router.get('/', verifyToken, requireRole('doctor'), getAll);
 // POST /api/patients/assign-doctor - asociere pacient-doctor
 router.post('/assign-doctor', verifyToken, requireRole('doctor'), assignDoctor);
 
+//DELETE /api/patients/unassign-doctor  dezasociere pacient-doctor
+router.delete('/unassign-doctor', verifyToken, requireRole('doctor'), unassignDoctor);
+
 // GET /api/patients/:id
 router.get('/:id', verifyToken, getById);
 
 // PUT /api/patients/:id
 router.put('/:id', verifyToken, requireRole('doctor'), update);
-
-//DELETE /api/patients/unassign-doctor  dezasociere pacient-doctor
-router.delete('/unassign-doctor', verifyToken, requireRole('doctor'), unassignDoctor);
 
 module.exports = router;
