62167dfbbf47b6a7374d7e2950c254d8ec97462d
PaulaTomo <paulatomoiaga91@yahoo.com>
2026-04-26 11:59:39 +0200

    Adaugat control pentru role si asociere automata user cu pacient sau doctor la inregistrare.

--- cloud-backend/controllers/authController.js
+++ cloud-backend/controllers/authController.js
@@ -2,19 +2,20 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { getPool, sql } = require('../config/db');
 
-// Înregistrare utilizator nou (medic sau pacient)
+// Inregistrare utilizator nou (medic sau pacient)
 async function register(req, res, next) {
   try {
-    const { email, password, role, firstName, lastName } = req.body;
+    const { email, password, role, firstName, lastName, specialization, clinicName,
+      age, weight, height, bloodType} = req.body;
     const pool = await getPool();
 
-    // Verifică dacă emailul există deja
+    // Verifica daca emailul exista deja
     const existing = await pool.request()
       .input('email', sql.NVarChar, email)
       .query('SELECT id FROM users WHERE email = @email');
 
     if (existing.recordset.length > 0) {
-      return res.status(409).json({ error: 'Emailul este deja înregistrat' });
+      return res.status(409).json({ error: 'Emailul este deja inregistrat' });
     }
 
     const passwordHash = await bcrypt.hash(password, 12);
@@ -32,13 +33,50 @@ async function register(req, res, next) {
       `);
 
     const user = result.recordset[0];
-    res.status(201).json({ user });
+    let doctor = null;
+    let patient = null;
+
+    if (role === 'doctor') {
+      const doctorResult = await pool.request()
+          .input('userId', sql.Int, user.id)
+          .input('specialization', sql.NVarChar, specialization || 'General')
+          .input('clinicName', sql.NVarChar, clinicName || '')
+          .query(`
+          INSERT INTO doctors (user_id, specialization, clinic_name)
+          OUTPUT INSERTED.id, INSERTED.specialization, INSERTED.clinic_name
+          VALUES (@userId, @specialization, @clinicName)
+        `);
+
+      doctor = doctorResult.recordset[0];
+    }
+
+    if (role === 'patient') {
+      const patientResult = await pool.request()
+          .input('userId', sql.Int, user.id)
+          .input('age', sql.Int, age || null)
+          .input('weight', sql.Decimal(5,2), weight || null)
+          .input('height', sql.Decimal(5,2), height || null)
+          .input('bloodType', sql.NVarChar, bloodType || null)
+          .query(`
+          INSERT INTO patients (user_id, age, weight, height, blood_type)
+          OUTPUT INSERTED.id, INSERTED.age, INSERTED.weight, INSERTED.height, INSERTED.blood_type
+          VALUES (@userId, @age, @weight, @height, @bloodType)
+        `);
+
+      patient = patientResult.recordset[0];
+    }
+
+    res.status(201).json({
+      user,
+      doctor,
+      patient
+    });
   } catch (err) {
     next(err);
   }
 }
 
-// Login — returnează JWT + date utilizator
+// Login — returneaza JWT + date utilizator
 async function login(req, res, next) {
   try {
     const { email, password } = req.body;
@@ -65,7 +103,7 @@ async function login(req, res, next) {
       { expiresIn: '24h' }
     );
 
-    // Returnează userul fără parolă
+    // Returnează userul fara parola
     const { password_hash, ...userWithoutPassword } = user;
     res.json({ token, user: userWithoutPassword });
   } catch (err) {
